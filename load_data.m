343cd626b9ec24cb4b66c0e0fd4fa3dfc15ee30c
Marcus Karpoff <mkarpoff@ualberta.ca>
2016-03-21 11:51:54 -0600

    Added files to load in data and generat box plots

---
+++ load_data.m
@@ -0,0 +1,39 @@
+function [ data ] = load_data()
+%LOAD_DATA Loads the low frequency data from 
+%   Loads the data into a structure matching the ones bellow
+% data = struct('numHouses', 0, 'house',[]);
+% house = struct('numChannels', 0, 'channel', [], 'name', '');
+% channel = struct('name', '', 'dim', 0, 'time', [], 'load', [], 'label','');
+    data = struct('numHouses', 0, 'house',[]);
+    for i = 1:6
+        % For debuging purposes can be removed
+        disp(['loading house ', num2str(i), '...'])
+        base = 'low_freq/';
+        house = struct('numChannels', 0, 'channel', [], 'name', '');
+        house.name = strcat('house_', num2str(i));
+        directory = dir(strcat(base, house.name, '/'));
+        for f = 1:size(directory,1)-2-1 %For the ./ ../ and label files
+            channel = struct('name', strcat('channel_', num2str(f), '.dat'), 'dim', 0, 'time', [], 'load', [], 'label','');
+            if exist(strcat(base, house.name,'/' ,channel.name), 'file') ~= 2
+                break
+            end
+            d = importdata(strcat(base, house.name,'/' ,channel.name));
+            channel.time = d(:,1);
+            channel.load = d(:,2);
+            channel.dim = size(d(:,2),1);
+            house.numChannels = house.numChannels + 1;
+            house.channel = horzcat(house.channel, channel);
+        end
+        filename= strcat(base, house.name,'/' , 'labels.dat');
+        delimiterIn = ' ';
+        labels = importdata(filename,delimiterIn);
+        for n = 1:size(labels)
+            a = strsplit(char(labels(n)));
+            house.channel(n).label = a(2);
+        end
+        data.numHouses = data.numHouses + 1;
+        data.house = horzcat(data.house, house);
+    end
+
+end
+
---
+++ make_boxplot.m
@@ -0,0 +1,18 @@
+function [] = make_boxplot( house )
+%MAKE_BOXPLOT takes a house data in a makes a boxplot of it separating out
+%the main data
+% House data should be a struct in the format bellow
+% house = struct('numChannels', 0, 'channel', [], 'name', '');
+% channel = struct('name', '', 'dim', 0, 'time', [], 'load', [], 'label','');
+    subplot(1,house.numChannels,[1,2])
+    loads = [house.channel(1).load house.channel(2).load];
+    labels = [house.channel(1).label house.channel(2).label];
+    boxplot(loads, 'Labels', labels, 'labelorientation','inline');
+    title([house.name, ' mains']);
+    subplot(1,house.numChannels,3:house.numChannels);
+    loads = [house.channel(3:house.numChannels).load];
+    labels = [house.channel(3:house.numChannels).label];
+    boxplot(loads, 'Labels', labels,'labelorientation','inline');
+    title([house.name, ' appliances']);
+end
+
