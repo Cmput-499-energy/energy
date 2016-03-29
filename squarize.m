ece892cb7db3a657a5e830f3c5cc9668f1be9b71
Jaden Travnik <travnik@cs.ualberta.ca>
2016-03-29 13:23:51 -0600

    Fixed smoothData and updated squarize to use changes

--- squarize.m
+++ squarize.m
@@ -5,29 +5,19 @@ function [ state, state_time  ] = squarize( load, time, uthresh, lthresh )
 % Calculate initial state.
 % assume it started in off state TODO this is probobaly a bad idea
 current_state = 0;
-current_time = time(1);
 state = [current_state];
-state_time = [current_time];
+state_time = time;
 % TODO this should be preallocated
 
 for i = 2:length(time);
-    current_load = load(i);
-    prev_time = time(i-1);
-    current_time = time(i);
-    if (current_state == 0)
-        if current_load >= uthresh
-            state = [state current_state];
-            state_time = [state_time prev_time current_time];
-            current_state = 1;
-            state = [state current_state];
-        end
-    else %(current_state == 1)
-        if current_load <= lthresh
-            state = [state current_state];
-            state_time = [state_time prev_time current_time];
-            current_state = 0;
-            state = [state current_state];
-        end
+    if load(i) >= uthresh,
+        state = [state, 1];
+        current_state = 1;
+    else if load(i) <= lthresh,
+        state = [state, 0];
+        current_state = 0;
+    else
+        state = [state, current_state];
     end
 end
 
