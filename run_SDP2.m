273e2089aa4e2e0fc07150a099b6553d5eba0549
touqir <touqir@ualberta.ca>
2016-04-15 11:55:10 -0600

    Added 18 appliance SPD inference states

--- run_SDP2.m
+++ run_SDP2.m
@@ -1,9 +1,10 @@
 %script for running on 18 appliances 
 
+cvx_solver SEDUMI
 t=500;
 load('Params1.mat');
 load('test_cases_18.mat');
 n=18;
-[states, x_vars, M_vars, errors]=run_SDP_approximation(Params, test_cases_18(10),t,n);
-save('SDP_prediction_18_10.mat', 'states', 'x_vars', 'M_vars', 'errors');	
+[states, x_vars, M_vars, errors]=run_SDP_approximation(Params, test_cases_18(1),t,n);
+save('SDP_prediction_18_1.mat', 'states', 'x_vars', 'M_vars', 'errors');	
 clear all
